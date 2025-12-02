07e6d881655dcc16e3ee42b1142cc0100e66ba48
Omkar Binage <omkarbinage0@gmail.com>
2025-12-02 16:59:39 +0530

    Configure Next.js Image component to allow backend server images

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -4,6 +4,30 @@ const nextConfig = {
   transpilePackages: ['react-map-gl', 'mapbox-gl'],
   // Note: Cannot use same packages in both transpilePackages and serverExternalPackages
   // Mapbox packages need to be transpiled for browser compatibility
+  
+  images: {
+    remotePatterns: [
+      // Development - localhost backend
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+      // Production - any HTTPS backend
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/uploads/**',
+      },
+      // Development - any HTTP backend (for other dev environments)
+      {
+        protocol: 'http',
+        hostname: '**',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
